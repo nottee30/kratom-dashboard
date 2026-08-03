9e36c44f5c4ec3894eb9f3f997cc3a8de4824f94
nottee30 <notteeac1@gmail.com>
2026-08-03 23:40:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -299,6 +299,7 @@ function showCustomer(name){
     if(rank===1) rankClass="gold";
     else if(rank===2) rankClass="silver";
     else if(rank===3) rankClass="bronze";
+    else if(rank===customers.length-1) rankClass="bottom";
     else if(rank===customers.length) rankClass="last";
 
     const percent =
