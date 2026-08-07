24f4f24e6c6e189cf78e4b1758213c9a8b68df98
nottee30 <notteeac1@gmail.com>
2026-08-07 16:04:17 +0700

    Update pay.js

--- pay.js
+++ pay.js
@@ -221,6 +221,34 @@ function showSuccess(){
     },3000);
 
 }
+document
+.getElementById("slip")
+.addEventListener("change", function(){
+
+    const file =
+    this.files[0];
+
+    if(!file) return;
+
+    const reader =
+    new FileReader();
+
+    reader.onload = function(e){
+
+        const preview =
+        document.getElementById("preview");
+
+        preview.src =
+        e.target.result;
+
+        preview.style.display =
+        "block";
+
+    };
+
+    reader.readAsDataURL(file);
+
+});
 window.onload = function () {
     loadPay();
 };
